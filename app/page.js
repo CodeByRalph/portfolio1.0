e7812b0b8d25162de1f3e051552a8827a8e224bb
Ralph Stout <ralph.stout14@icloud.com>
2024-07-26 21:28:25 -0500

    Commit

--- app/page.js
+++ app/page.js
@@ -4,8 +4,6 @@ import Experience from "@/components/Experience";
 import Contact from "@/components/Contact";
 
 export default function Home() {
-  // Socials
-  // Linkedin, Github,
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Hero />
