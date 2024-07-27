6129ec0443386ff3231c0d7f55a74d7ab0f8595e
Ralph Stout <ralph.stout14@icloud.com>
2024-07-27 11:34:41 -0500

    Implemented emailjs to contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,11 +1,49 @@
+'use client'
+
+import { useRef } from 'react'
+import emailjs from "@emailjs/browser";
+
 export default function Contact() {
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const messageRef = useRef()
+
+  const sendMessage = (e) => {
+    e.preventDefault()
+    const templateParams = {
+      from_name: nameRef.current.value,
+      sender_email: emailRef.current.value,
+      message: messageRef.current.value
+    };
+    
+    emailjs.send(
+        "service_adpnnnd",
+        "template_vx53dxn",
+        templateParams,
+        "hdJACtUqL0Wgg9RC9"
+    )
+    .then(
+      function (response) {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Thanks, message sent successfully");
+      },
+      function (error) {
+        alert("OOPs something went wrong... Try again later");
+        console.log("FAILED...", error);
+      }
+    );
+    nameRef.current.value = ""
+    emailRef.current.value = ""
+    messageRef.current.value = ""
+  };
+
   return (
     <section id="contact" className="w-11/12 mx-auto my-10 p-5 lg:w-1/3">
       <h1 className="text-4xl mb-5 font-bold text-saffron">Let&apos;s Connect</h1>
       <p className="mb-5 font-light">
       If you want to know more about me or my work, or if you would just like to say hello, send me a message. I&apos;d love to hear from you.
       </p>
-      <form>
+      <form onSubmit={sendMessage}>
         <div className="mb-4">
           <label className="block text-sm mb-1 font-medium" htmlFor="name">
             Name *
@@ -13,7 +51,10 @@ export default function Contact() {
           <input
             type="text"
             id="name"
+            name="name"
+            ref={nameRef}
             className="w-full px-3 py-2 bg-lavender-blush border-b-2 border-black focus:outline-none"
+            required
           />
         </div>
         <div className="mb-4">
@@ -23,7 +64,10 @@ export default function Contact() {
           <input
             type="email"
             id="email"
+            name="email"
+            ref={emailRef}
             className="w-full px-3 py-2 bg-lavender-blush border-b-2 border-black focus:outline-none"
+            required
           />
         </div>
         <div className="mb-4">
@@ -32,9 +76,12 @@ export default function Contact() {
           </label>
           <textarea
             id="message"
+            name="message"
             className="w-full px-3 py-2 bg-lavender-blush border-b-2 border-black focus:outline-none"
             placeholder="Hello, how are you!"
             rows="4"
+            ref={messageRef}
+            required
           ></textarea>
         </div>
         <button
